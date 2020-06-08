5c68e885a8d55fc4483ec60a6051c0dd016f3095
fluffy <fluffy@beesbuzz.biz>
2020-06-07 21:09:24 -0700

    Flask wrapper coverage

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,32 @@
 """ Common functions for the test routines """
 import logging
 
+from authl import handlers
+
 logging.basicConfig(level=logging.DEBUG)
+
+
+class TestHandler(handlers.Handler):
+    """ null test handler that does nothing """
+
+    @property
+    def cb_id(self):
+        return "nothing"
+
+    def initiate_auth(self, id_url, callback_uri, redir):
+        raise ValueError("not implemented")
+
+    def check_callback(self, url, get, data):
+        raise ValueError("not implemented")
+
+    @property
+    def service_name(self):
+        return "Nothing"
+
+    @property
+    def url_schemes(self):
+        return []
+
+    @property
+    def description(self):
+        return "Does nothing"
